c310c455420ca0efcfe599d3676d7989555374ac
fireflyy85 <fireflyy85@gmail.com>
2017-06-28 09:55:29 -0400

    Add files via upload

---
+++ Lab 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            bool isOdd = false;
+            Console.WriteLine("Enter an integer between 1 and 100");
+            input = int.Parse( Console.ReadLine());
+            if (input % 2 == 1)
+            {
+                isOdd = true;
+                Console.WriteLine(input +" is odd");
+
+            }
+            if (isOdd == false && input >= 2 && input < 25)
+            {
+                Console.WriteLine("Even and Less than 25");
+            }
+            else if (!isOdd && input >= 26 && input <= 60)
+            {
+                Console.WriteLine("Even");
+            }
+            else if (!isOdd && input >= 60)
+            {
+                Console.WriteLine(input + " Even");
+            }
+            else if (isOdd && input > 60)
+            {
+                Console.WriteLine(input + " Odd");
+            }
+            
+        }
+    }
+}
